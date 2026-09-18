403572424f8b68854cb79b06ea1a3d00b053f602
tgadziev220-boop <tgadziev220@gmail.com>
2026-09-18 17:21:57 +0300

    Add files via upload
    Обновление CryptoArb v1.0

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-const CACHE='crypto-arb-v02';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='cryptoarb-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
